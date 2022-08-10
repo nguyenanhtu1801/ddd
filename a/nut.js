372137aee201df4eb9b8f189fe006bfdede12eba
nguyenanhtu1801 <36001614+nguyenanhtu1801@users.noreply.github.com>
2022-08-11 00:23:30 +0700

    Create nut.js

---
+++ a/nut.js
@@ -0,0 +1,35 @@
+data.forEach(function(item){
+   var option = document.createElement('option');
+   option.value = item.id;
+   option.innerHTML = item.name;
+   selector.appendChild(option)
+})
+
+function hu(){
+  var x = document.getElementById("selector").value;
+  var b = data[x].img
+  document.getElementById("ola").innerHTML = b;
+}
+
+function chaptruoc(){
+  var x = document.getElementById("selector").value;
+  var b = Number(x)-1;
+  if (b > -1) {
+  var z = data[b].img
+  var $select = document.querySelector('#selector');
+  $select.value = b;
+  document.getElementById("ola").innerHTML = z;
+    document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0
+};};
+
+function chapsau(){
+  var x = document.getElementById("selector").value;
+  var b = Number(x)+ 1;
+  var z = data[b].img
+  var $select = document.querySelector('#selector');
+  $select.value = b;
+  document.getElementById("ola").innerHTML = z;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0
+};
